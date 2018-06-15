fc0a385b89fb6421b31058aee4dafe7d6baf5efe
ayush@251196 <git2511ayush@gmail.com>
2018-06-15 23:57:13 +0530

    Splash screen is ready

--- app/src/main/java/com/knowlexon/mycatercan/mywatercan/Activity/SplashScreenActivity.java
+++ app/src/main/java/com/knowlexon/mycatercan/mywatercan/Activity/SplashScreenActivity.java
@@ -1,7 +1,12 @@
 package com.knowlexon.mycatercan.mywatercan.Activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.knowlexon.mycatercan.mywatercan.R;
@@ -17,11 +22,16 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        //Hre is my second commit
-        //Hre is my third commit
+        ImageView rd = (ImageView) findViewById(R.id.round_drop);
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_clockwise);
+        rd.startAnimation(hyperspaceJumpAnimation);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run()
+            {
+                finish();
+            }
+        },2000);
 
     }
 
